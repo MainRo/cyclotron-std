2f2f2e81f7fe35b2dc5582a4ffb1b72a848df424
Romain Picard <romain.picard@oakbits.com>
2018-04-11 18:39:30 +0200

    stdout: fixed implementation

--- cyclotron_std/sys/stdout.py
+++ cyclotron_std/sys/stdout.py
@@ -1,12 +1,14 @@
 import sys
+from collections import namedtuple
+
 from cyclotron import Component
 
 Sink = namedtuple('Sink', ['data'])
 
-def make_stdout_driver(loop = None):
+def make_driver(loop = None):
 
     def stdout_driver(sink):
-        sink.data.subscribe(lambda data: sys.stdout.write(data))
+        sink.data.subscribe(lambda data: sys.stdout.write(bytes(data, 'utf-8').decode('unicode_escape')))
         return None
 
     return Component(call=stdout_driver, input=Sink)
