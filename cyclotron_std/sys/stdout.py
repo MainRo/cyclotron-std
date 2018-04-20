c563545587d3bbb08ffae6eaddef4033eca42ca8
Romain Picard <romain.picard@oakbits.com>
2018-04-20 17:56:39 +0200

    stdout: error management and flush when sink is completed

--- cyclotron_std/sys/stdout.py
+++ cyclotron_std/sys/stdout.py
@@ -5,10 +5,21 @@ from cyclotron import Component
 
 Sink = namedtuple('Sink', ['data'])
 
-def make_driver(loop = None):
+
+def make_driver(loop=None):
 
     def stdout_driver(sink):
-        sink.data.subscribe(lambda data: sys.stdout.write(bytes(data, 'utf-8').decode('unicode_escape')))
+        def on_data_error(e):
+            sys.stdout.write("error: {}".format(e))
+            sys.stdout.flush()
+
+        def on_data_completed():
+            sys.stdout.flush()
+
+        sink.data.subscribe(
+            on_next=lambda data: sys.stdout.write(bytes(data, 'utf-8').decode('unicode_escape')),
+            on_error=on_data_error,
+            on_completed=on_data_completed)
         return None
 
     return Component(call=stdout_driver, input=Sink)
